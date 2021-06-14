3e18f2e8c66507703c14f8cf33b1368d43863691
NehaK745 <nehakumari1461@gmail.com>
2021-06-14 10:10:42 +0530

    first commit

---
+++ client/src/pages/CreatePost/CreatePost.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import './CreatePost.css';
+import {useHistory} from "react-router-dom";
+
+function CreatePost() {
+  let history = useHistory();
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a Title!"),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+     history.push("/");
+    });
+  };
+ 
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <ErrorMessage name="title" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="title"
+            placeholder="Title"
+          />
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="postText"
+            placeholder="Post"
+          />
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="Username"
+          />
+
+          <button type="submit"> Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default CreatePost;
